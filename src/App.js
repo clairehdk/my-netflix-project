f59277a270a52a4c1110dedfe429fb92eb49597e
epidia <clairon_6@hotmail.fr>
2021-04-27 15:47:45 +0200

    end

--- src/App.js
+++ src/App.js
@@ -5,23 +5,14 @@ import movies from "./movies.json";
 
 // Composants
 
-// import Section from "./components/Section";
+import Section from "./components/Section";
 
 function App() {
   return (
     <div class="container">
       <img class="logo" src={logo} />
       {movies.map((elem, index) => {
-        return (
-          <div class="block">
-            <h2>{elem.category}</h2>
-            <div class="cat">
-              {elem.images.map((image, index) => {
-                return <img src={image}></img>;
-              })}
-            </div>
-          </div>
-        );
+        return <Section category={elem.category} images={elem.images} />;
       })}
     </div>
   );
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,12 +1,16 @@
-// import movies from "../movies.json";
+const Section = ({ category, images }) => {
+  return (
+    <div className="container">
+      <div class="block">
+        <h2>{category}</h2>
+        <div class="cat">
+          {images.map((image, index) => {
+            return <img src={image}></img>;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-// const Section = ({ category, images }) => {
-//   return (
-//     <div className="container">
-//       <h2>{category}</h2>
-//       {/* <img src={images} /> */}
-//     </div>
-//   );
-// };
-
-// export default Section;
+export default Section;
